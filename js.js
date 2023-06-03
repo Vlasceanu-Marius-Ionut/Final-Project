de221068f29737501b83d922559a97d3764fa426
Vlasceanu Marius-Ionut <128095775+Vlasceanu-Marius-Ionut@users.noreply.github.com>
2023-06-03 23:03:36 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
 const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git').exports;
 const git = gitExtension.getAPI(1);
+
+const burgerMenu = document.querySelector('.burger-menu');
+const menuOverlay = document.querySelector('.menu-overlay');
+
+burgerMenu.addEventListener('click', () => {
+  document.body.classList.toggle('menu-open');
+});
+
+menuOverlay.addEventListener('click', () => {
+  document.body.classList.remove('menu-open');
+});
+//-----------------------------------------------//
