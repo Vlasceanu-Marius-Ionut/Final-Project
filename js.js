53cc9cc16356233e06aa88cac162d12f7d17e203
Marius <vlasceanu_marius117@yahoo.com>
2023-06-07 23:30:04 +0300

    A lot of changes

--- js.js
+++ js.js
@@ -8,3 +8,20 @@ burgerMenu.addEventListener('click', () => {
 menuOverlay.addEventListener('click', () => {
   document.body.classList.remove('menu-open');
 });
+
+function addClickListenerToItems() {
+  const itemClasses = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6']; 
+
+  itemClasses.forEach(itemClass => {
+    const items = document.querySelectorAll(`.${itemClass}`);
+
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        window.location.href = 'http://127.0.0.1:5500/CoffeeWorld1.html';
+      });
+    });
+  });
+}
+
+addClickListenerToItems();
+
